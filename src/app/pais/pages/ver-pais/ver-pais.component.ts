2a756c15764040179bc389e925dbfe89241d3849
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 16:07:17 -0500

    Se agrego la pantalla para ver información del pais seleccionado

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from "rxjs/operators";
+import { switchMap,tap } from "rxjs/operators";
+import { RESTCountriesResponse } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,17 +11,17 @@ import { switchMap } from "rxjs/operators";
   ]
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { }
+  
+  pais!: RESTCountriesResponse;
+  
+  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService, ) { }
 
   ngOnInit(): void {
 
     this.activatedRoute.params
       //SwitchMap permite tomar un observer y regresar otro
-      .pipe(switchMap((params) => this.paisService.getPaisXCodigo(params.id)))
-      .subscribe(resp => {
-        console.log(resp);
-      });
+      .pipe(switchMap((params) => this.paisService.getPaisXCodigo(params.id)), tap(console.log)) //El tap nos imprime la respuesta 
+      .subscribe(pais=>this.pais=pais);
     //Sin SwitchMap este seria el codigo
     // this.activatedRoute.params.subscribe(params => {
     //   this.paisService.getPaisXCodigo(params.id).subscribe(pais => {
