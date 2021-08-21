845473cff6832b1650e90b694ef67d38f1d3ac37
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 13:50:33 -0500

    Se agregó el tipado e interfaces de paises.response obtenidas del endpoint

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { RESTCountriesResponse } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class PaisService {
  
   constructor(private http:HttpClient) {}
 
-  buscarPais(termino:string): Observable<any>{
+  buscarPais(termino:string): Observable<RESTCountriesResponse []>{
     const url = `${this._apiUrl}/name/${termino}`
-    return this.http.get(url);
+    return this.http.get<RESTCountriesResponse[]>(url);
   }
 }
