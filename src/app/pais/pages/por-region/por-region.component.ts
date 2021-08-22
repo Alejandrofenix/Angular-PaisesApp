52925d6acdf67f7daa829bf5074f44031f9e3a37
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 20:04:53 -0500

    Se establecio la conexión con el API para obtener los paises x región

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { RESTCountriesResponse } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
@@ -15,20 +16,31 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorRegionComponent {
 
-  regiones: string[] = ["africa", "america", "asia", "europe", "oceania"];
+  regiones: string[] = ["africa", "americas", "asia", "europe", "oceania"];
   regionActiva: string = '';
+  paises: RESTCountriesResponse[] = [];
+  termino: string = '';
+  hayError: boolean = false;
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   getClaseCSS(region: string): string {
     return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
 
   }
   activarRegion(region: string) {
+    if (region == this.regionActiva) return;
     this.regionActiva = region;
+    this.paisService.buscarXRegion(region).subscribe(paises => this.paises = paises);
 
     //TODO: Hacer el llamado al servicio
   }
+
+
+
+
+
+
 
 
 }
