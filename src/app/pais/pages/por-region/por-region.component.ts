1ff6faca8da7aa1e6bed1eb8b68ba021fc3c58b3
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 19:50:31 -0500

    Se agregó la interfaz para Buscar por Región

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -6,34 +6,29 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+    button{
+      margin-right:5px;
+    }
+    `
   ]
 })
-export class PorRegionComponent{
+export class PorRegionComponent {
 
+  regiones: string[] = ["africa", "america", "asia", "europe", "oceania"];
+  regionActiva: string = '';
 
-  termino: string ='';
-  hayError: boolean = false;
-  sinPaises:boolean = false;
-  paises:RESTCountriesResponse[]=[];
-  constructor(private paisService:PaisService) { }
+  constructor() { }
 
-  buscar( termino: string){
-  
-    this.hayError=false;
-    this.termino=termino;
-    this.paises=[];
-    this.paisService.buscarXRegion(this.termino).subscribe(resp=>{
-      this.paises=resp;
-      this.sinPaises=false;
-    },(err)=>{
-      this.hayError=true;
-      
-    });
+  getClaseCSS(region: string): string {
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
 
   }
-  
-  sugerencias(termino:string){
-    this.hayError=false;
-    //TODO: crear sugerencias
+  activarRegion(region: string) {
+    this.regionActiva = region;
+
+    //TODO: Hacer el llamado al servicio
   }
+
+
 }
