eee400779192ab72679b73bb1dc536ea396d25a5
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 21:09:01 -0500

    Se agregarón optimizaciones a los endpointsW

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RESTCountriesResponse } from '../interfaces/pais.interface';
 
@@ -9,22 +9,26 @@ import { RESTCountriesResponse } from '../interfaces/pais.interface';
 export class PaisService {
 
   private  _apiUrl: string ='https://restcountries.eu/rest/v2'
+  get httpParams(){
+    return new HttpParams().set('fields','name;capital;alpha2Code;flag;population');
+  }
  
   constructor(private http:HttpClient) {}
 
   buscarPais(termino:string): Observable<RESTCountriesResponse []>{
     const url = `${this._apiUrl}/name/${termino}`
-    return this.http.get<RESTCountriesResponse[]>(url);
+    return this.http.get<RESTCountriesResponse[]>(url, {params: this.httpParams});
   }
 
   buscarXCapital(termino:string): Observable<RESTCountriesResponse []>{
     const url = `${this._apiUrl}/capital/${termino}`
-    return this.http.get<RESTCountriesResponse[]>(url);
+    return this.http.get<RESTCountriesResponse[]>(url, {params: this.httpParams});
   }
 
   buscarXRegion(termino:string): Observable<RESTCountriesResponse []>{
+   
     const url = `${this._apiUrl}/region/${termino}`
-    return this.http.get<RESTCountriesResponse[]>(url);
+    return this.http.get<RESTCountriesResponse[]>(url, {params: this.httpParams});
   }
 
   getPaisXCodigo(id:string): Observable<RESTCountriesResponse >{
